6204486f503c53711a2714d88686d3b6eb40c7df
Martin Němeček <m_nemecek@utb.cz>
2023-12-30 20:08:44 +0100

    Changed saveData. Added back button.

--- app/src/main/java/com/example/playercount/action/ActionViewModel.kt
+++ app/src/main/java/com/example/playercount/action/ActionViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.playercount.action
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.playercount.Repository
+import com.example.playercount.main.MainActivity
+import com.example.playercount.mmo.MMOActivity
 import com.example.playercount.model.Contents
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,13 +23,22 @@ class ActionViewModel(
     val file: String = "playerCounter.txt"
 
     private val _subjectInfoValue = MutableLiveData<Contents>()
+    private val _nameOfTheGame = MutableLiveData<String>("Vyber si hru")
     val subjectInfoValue: LiveData<Contents> = _subjectInfoValue
+    val nameOfTheGame: LiveData<String> = _nameOfTheGame
 
-    fun getSubjectInfo(appid: Int){
+
+    fun changeActivityToMain() {
+        val intent = Intent(packageContext, MainActivity::class.java)
+        ContextCompat.startActivity(packageContext, intent, null)
+    }
+
+    fun getSubjectInfo(appid: Int, gameName: String){
         viewModelScope.launch(Dispatchers.IO) {
             try{val result = repository.getSubjectInfo(appid)
                 _subjectInfoValue.postValue(result)
-                saveData(result.toString())
+                _nameOfTheGame.postValue(gameName)
+                saveData(result, gameName)
                 }
             catch (e: Exception){
                 Log.i("TEST", "stala se chyba v SubjectInfo v Action" + e.toString())
@@ -34,11 +47,13 @@ class ActionViewModel(
     }
 
 
-    fun saveData(data: String) {
+    fun saveData(data: Contents?, gameName: String) {
         val fileOutputStream: FileOutputStream
         try{
             fileOutputStream = packageContext.openFileOutput(file, Context.MODE_PRIVATE)
-            fileOutputStream.write(data.toByteArray())
+            if (data != null) {
+                fileOutputStream.write("${data.response.player_count.toString()} hráčů v ${gameName}".toByteArray())
+            }
         }
         catch (e: Exception){
             Log.i("TEST", "stala se chyba v saveData v Action " + e.toString())
--- app/src/main/java/com/example/playercount/mmo/MMOViewModel.kt
+++ app/src/main/java/com/example/playercount/mmo/MMOViewModel.kt
@@ -10,7 +10,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.*
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.playercount.main.MainActivity
 
 class MMOViewModel(
     private val repository: Repository,
@@ -19,13 +22,21 @@ class MMOViewModel(
 
     val file: String = "playerCounter.txt"
     private val _subjectInfoValue = MutableLiveData<Contents>()
+    private val _nameOfTheGame = MutableLiveData<String>("Vyber si hru")
     val subjectInfoValue: LiveData<Contents> = _subjectInfoValue
+    val nameOfTheGame: LiveData<String> = _nameOfTheGame
 
-    fun getSubjectInfo(appid: Int) {
+    fun changeActivityToMain() {
+        val intent = Intent(packageContext, MainActivity::class.java)
+        ContextCompat.startActivity(packageContext, intent, null)
+    }
+
+    fun getSubjectInfo(appid: Int, gameName: String) {
         viewModelScope.launch(Dispatchers.IO) {
             try{val result = repository.getSubjectInfo(appid)
                 _subjectInfoValue.postValue(result)
-                saveData(result.toString())
+                _nameOfTheGame.postValue(gameName)
+                saveData(result, gameName)
             }
             catch (e: Exception){
                 Log.i("TEST", "stala se chyba v SubjectInfo v MMO " + e.toString())
@@ -34,42 +45,16 @@ class MMOViewModel(
     }
 
 
-    fun saveData(data: String) {
+    fun saveData(data: Contents?, gameName: String) {
         val fileOutputStream: FileOutputStream
         try{
             fileOutputStream = packageContext.openFileOutput(file, Context.MODE_PRIVATE)
-            fileOutputStream.write(data.toByteArray())
+            if (data != null) {
+                fileOutputStream.write("${data.response.player_count.toString()} hráčů v ${gameName}".toByteArray())
+            }
         }
         catch (e: Exception){
             Log.i("TEST", "stala se chyba v saveData v MMO " + e.toString())
         }
     }
-
-
-        /*
-        val fileOutputStream: FileOutputStream
-        try{
-            fileOutputStream = openFileOutput("test.txt",Context.MODE_PRIVATE)
-            fileOutputStream.write(bg3.toByteArray())
-            Toast.makeText(this,"Data Saved",Toast.LENGTH_SHORT).show()
-        }catch(e: Exception){
-            Toast.makeText(this,"Chyba",Toast.LENGTH_SHORT).show()
-        }
-
-         */
-
-        /*
-        var fileInputStream: FileInputStream? = null
-        fileInputStream= openFileInput("test.txt")
-        var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
-        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
-        val stringBuilder: StringBuilder = StringBuilder()
-        var text: String? = null
-        while ({text = bufferedReader.readLine(); text}() != null){
-            stringBuilder.append(text)
-        }
-        Toast.makeText(this,stringBuilder.toString(),Toast.LENGTH_LONG).show()
-
-         */
-
 }
